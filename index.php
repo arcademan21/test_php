e6f6dfbe2fc577a706092786ca04adf6360dc67a
arcademan <haroldyarturo@gmail.com>
2020-09-09 16:37:25 +0200

    Añadi ajax al archivo index y cree el earchivo request.php

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
-<?php  echo 'Hello world'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+</head>
+<body>
+
+	<script type="text/javascript">
+		
+		$.ajax({
+			url: 'request.php',
+			type: 'POST',
+			data: {
+				nombre: 'Harold'
+			},
+
+			success: function(response){
+				console.log(response)
+			}
+		})
+
+	</script>
+
+</body>
+</html>
